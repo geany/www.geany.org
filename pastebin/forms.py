694f430a2b7a05a1fc1aa4490c888b17337be4d2
Enrico Tröger <enrico.troeger@uvena.de>
2013-05-23 21:55:39 +0200

    Use RadioSelect as widget for the snippet expire options

--- pastebin/forms.py
+++ pastebin/forms.py
@@ -3,12 +3,12 @@
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -47,6 +47,7 @@ class SnippetForm(forms.ModelForm):
         choices=EXPIRE_CHOICES,
         initial=EXPIRE_DEFAULT,
         label=_(u'Expires'),
+        widget=forms.RadioSelect,
     )
 
     #----------------------------------------------------------------------
