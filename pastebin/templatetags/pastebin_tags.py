1f80507adc4023a1d4520c72c7ed37859b6b2702
Enrico Tröger <enrico.troeger@uvena.de>
2013-10-30 22:55:11 +0100

    Show snippet TTL as forever if it is (almost) forever

--- pastebin/templatetags/pastebin_tags.py
+++ pastebin/templatetags/pastebin_tags.py
@@ -3,17 +3,21 @@
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.template import Library
+from django.template.defaultfilters import timeuntil
+from django.utils.timezone import now
 from pastebin.highlight import pygmentize
+
+NINETY_YEARS_IN_DAYS = 32850  # 90 * 365
 
 
 register = Library()
@@ -23,6 +27,18 @@ register = Library()
 @register.filter
 def in_list(value, arg):
     return value in arg
+
+
+#----------------------------------------------------------------------
+@register.filter
+def timeuntil_or_forever(snippet_expire):
+    ttl = snippet_expire - now()
+    if ttl.days > NINETY_YEARS_IN_DAYS:
+        # snippet TTL 'forever' is defined as 100 years, so if remaining TTL is more than
+        # (90 * 365) days, we most probably got a snippet with TTL 'forever'
+        return u'forever'
+    else:
+        return timeuntil(snippet_expire)
 
 
 #----------------------------------------------------------------------
