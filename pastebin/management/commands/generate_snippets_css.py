d3b2a97510fdceb8aeed486d571cc2e3feadc614
Enrico Tröger <enrico.troeger@uvena.de>
2015-10-25 22:51:11 +0100

    Migrate Pygments CSS update script into a management command
    Same behavior but now cleanly integrated into Django as management command:
    python manage.py generate_snippets_css

--- pastebin/management/commands/generate_snippets_css.py
+++ pastebin/management/commands/generate_snippets_css.py
@@ -1,25 +1,28 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
 # LICENCE: This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from django.core.management.base import BaseCommand
 from pygments.formatters import HtmlFormatter
 
 
-f = open('pastebin/static/css/pygments.css', 'w')
+########################################################################
+class Command(BaseCommand):
+    help = "Regenerate CSS for snippet sxntax highlighting py Pygments"
+    requires_system_checks = False
 
-# You can change style and the html class here:
-f.write(HtmlFormatter(style='colorful').get_style_defs('.code'))
-
-f.close()
+    #----------------------------------------------------------------------
+    def handle(self, *args, **options):
+        with open('pastebin/static/css/pygments.css', 'w') as css_file:
+            # You can change style and the html class here:
+            css_file.write(HtmlFormatter(style='colorful').get_style_defs('.code'))
