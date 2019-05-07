21db0c4ce08e4afa982028b7adef4aa6d12e567f
Enrico Tröger <enrico.troeger@uvena.de>
2019-05-07 21:45:33 +0200

    Increase upload file size to allow larger uploads
    The default upload file size is 2.5 MB but nowadays even images like to
    be a bit larger.

--- geany/settings.py
+++ geany/settings.py
@@ -183,6 +183,7 @@ AUTHENTICATION_BACKENDS = ("mezzanine.core.auth_backends.MezzanineBackend",)
 # The numeric mode to set newly-uploaded files to. The value should be
 # a mode you'd pass directly to os.chmod.
 FILE_UPLOAD_PERMISSIONS = 0o644
+FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10 MB
 
 
 ADMINS = (('Enrico Tröger', 'enrico.troeger@uvena.de'),)
